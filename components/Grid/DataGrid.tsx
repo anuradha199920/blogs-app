d5615fbd3deb1b1558639c1942ebf41814e0e723
Eekeyguy <divyanshu123123@gmail.com>
2023-08-22 01:04:26 +0530

    formatted grid

--- components/Grid/DataGrid.tsx
+++ components/Grid/DataGrid.tsx
@@ -13,7 +13,7 @@ const convertTablePropsToMonthColumnDefs: ColDef[] = [
     { field: 'monthnftTraded', headerName: '30D NFTs Traded',  wrapHeaderText: true },
     { field: 'monthBuyers', headerName: '30D Buyers',  wrapHeaderText: true},
     { field: 'monthSellers', headerName: '30D Sellers',  wrapHeaderText: true},
-    { field: 'monthVolume', headerName: '30D Volume',valueFormatter:(params: ValueFormatterParams)=>(`${(params.value/1000).toFixed(2)}K`) },
+    { field: 'monthVolume', headerName: '30D Volume',valueFormatter:(params: ValueFormatterParams)=>(`${(params.value/1000000).toFixed(2)}M`) },
     { field: 'monthWashVolume', headerName: '30D Wash Volume', valueFormatter:(params: ValueFormatterParams)=>(`${(params.value/1000).toFixed(2)}K`)},
     { field: 'monthHighestSale', headerName: '30D Highest Sale', valueFormatter:(params: ValueFormatterParams)=>(params.value?? 0).toFixed(2)},
     { field: 'diamondHands', headerName: 'Diamond Hands',valueFormatter:(params: ValueFormatterParams)=>(`${(params.value?? 0).toFixed(2)}%`)},
@@ -82,28 +82,28 @@ const  DataGrid = ({props}:{props: TableProps[]}) => {
         <div className="col-span-12 rounded-sm border border-stroke bg-[#E4E3D8] px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 h-[80vh] w-full">
             <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
                 <div className="flex w-full flex-wrap gap-3 sm:gap-5">
-                <div className="flex min-w-47.5">
-                <h4 className="text-xl font-semibold text-black dark:text-white">
-                        NFT Market Stats
-                </h4>
-                </div>
+                    <div className="flex min-w-47.5">
+                    <h4 className="text-xl font-semibold text-black dark:text-white">
+                            NFT Market Stats
+                    </h4>
+                    </div>
                 </div>
                 <div className="flex w-full max-w-45 justify-end">
-                <div className="inline-flex items-center rounded-md bg-whiter p-1.5 dark:bg-meta-4">
-                    <button className={`${value=="Day"? "dark:bg-boxdark shadow-card  bg-white": ""} rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark`} onClick={handleButtonClick} value={"Day"}>
-                    1D
-                    </button>
-                    <button className={`${value=="Week"? "dark:bg-boxdark shadow-card  bg-white": ""} rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark`} onClick={handleButtonClick} value={"Week"}>
-                    7D
-                    </button>
-                    <button className={`${value=="Month"? "dark:bg-boxdark shadow-card  bg-white": ""} rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark`} onClick={handleButtonClick} value={"Month"}>
-                    30D
-                    </button>
+                    <div className="inline-flex items-center rounded-md bg-whiter p-1.5 dark:bg-meta-4">
+                        <button className={`${value=="Day"? "dark:bg-boxdark shadow-card  bg-white": ""} rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark`} onClick={handleButtonClick} value={"Day"}>
+                        1D
+                        </button>
+                        <button className={`${value=="Week"? "dark:bg-boxdark shadow-card  bg-white": ""} rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark`} onClick={handleButtonClick} value={"Week"}>
+                        7D
+                        </button>
+                        <button className={`${value=="Month"? "dark:bg-boxdark shadow-card  bg-white": ""} rounded py-1 px-3 text-xs font-medium text-black hover:bg-white hover:shadow-card dark:text-white dark:hover:bg-boxdark`} onClick={handleButtonClick} value={"Month"}>
+                        30D
+                        </button>
+                    </div>
                 </div>
             </div>
-      </div>
-        <div className="w-full h-[90%]">
-            <div
+            <div className="w-full h-[90%]">
+                <div
                 id="myGrid"
                 className="ag-theme-alpine"
                 style={{
@@ -118,8 +118,8 @@ const  DataGrid = ({props}:{props: TableProps[]}) => {
                             return {
                                 outerWidth: '100%'
                             };
-                    },
-                }}
+                            },
+                        }}
                     defaultColDef={{
                         filter: true,
                         wrapHeaderText: true,
@@ -127,6 +127,7 @@ const  DataGrid = ({props}:{props: TableProps[]}) => {
                         filterParams: {
                             newRowsAction: 'keep'
                         },
+                        autoHeaderHeight: true,
                         menuTabs: ['filterMenuTab', 'generalMenuTab', 'columnsMenuTab']
                     }}
                     columnDefs={columnDefs}
