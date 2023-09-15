63bbc9557d35af4f905a78e06e94f95d0cddd3e7
Eekeyguy <divyanshu123123@gmail.com>
2023-09-16 01:10:39 +0530

    .

--- components/dashboard/LatestStats.tsx
+++ components/dashboard/LatestStats.tsx
@@ -13,8 +13,8 @@ export default async function LatestStats(){
                         {duneDashboard.map(duneDashboard =>(
                             <Link href={`/lateststats/${duneDashboard.slug}`} className="cursor-pointer">
                                 <div className="inline-block px-3">
-                                <div className="rounded-md border border-stroke py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark align-middle">                                        
-                                    <div className="flex flex-wrap items-start sm:flex-nowrap sm:space-x-6 align-middle">
+                                    <div className="rounded-md border border-stroke py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark align-middle w-[400px]">                                        
+                                        <div className="flex flex-wrap items-start sm:flex-nowrap sm:space-x-6">
                                             <div className="relative mt-1 h-24 w-24 flex-shrink-0">  
                                                 <Image
                                                     src={duneDashboard.featuredImage.url}
@@ -24,12 +24,10 @@ export default async function LatestStats(){
                                                     sizes="96px"
                                                 />
                                             </div>
-                                            <div>
-                                                <div className="mb-3">
+                                            <div className="mb-3 h-24">
                                                 <h3 className="text-xl font-bold text-gray-800 dark:text-gray-300">
                                                     {duneDashboard.name}
                                                 </h3>
-                                                </div>
                                             </div>
                                         </div>
                                     </div>
