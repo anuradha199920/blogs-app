2d0a2921506449a61eb9ec170685ec129401d5c3
Eekeyguy <divyanshu123123@gmail.com>
2023-09-18 03:11:30 +0530

    .

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -20,7 +20,7 @@ const mobilemenu = [
     href: "/home#clients"
   },];
   return (
-    <div className="sticky top-0 z-10  py-2 w-[100%] border-stoke bg-white bg-opacity-90 mb-4 h-15  border-b-[1px] shadow-md">
+    <div className="sticky top-0 z-20  py-2 w-[100%] border-stoke bg-white bg-opacity-90 mb-4 h-15  border-b-[1px] shadow-md">
       <nav className="rounded-none px-4 lg:px-8 flex justify-between">
         <Disclosure key={"navBarDisclosure"}>
           {({ open }) => (
