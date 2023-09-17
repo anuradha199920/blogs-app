13306563c7341f37054a26407de344f18cfc3829
Eekeyguy <divyanshu123123@gmail.com>
2023-09-18 03:01:48 +0530

    .

--- components/blogs/BlogsSection.tsx
+++ components/blogs/BlogsSection.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 const BlogsSection: React.FC = () => {
     return (
         <>
-        <div className="sm:px-0  px-2 py-2 xl:px-10 mx-6 w-full">
+        <div className="sm:px-0 py-2 mx-[1%] w-[98%]">
             <div className="w-full flex items-center justify-center">
-                <h2 className="text-3xl font-semibold stylish-font text-[#3C61BD] lg:text-4xl lg:leading-tight tracking-tighter">
+                <h2 className="text-3xl font-semibold stylish-font text-[#3C61BD] lg:text-4xl lg:leading-tight tracking-tighter mb-5">
                     Weekly Blogs
                 </h2>
             </div>
@@ -15,11 +15,9 @@ const BlogsSection: React.FC = () => {
                 <Link href={"/archive"}>
                 <div className='rounded-full bg-sky-100 p-5'>Load More...</div>
                 </Link>
-
-            
             </div>
         </div>  
         </>
-    );
+    )
 };
 export default BlogsSection;
