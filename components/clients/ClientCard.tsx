6f7b9a99d7699b04fa780ff929c0a4acf1121b3d
Eekeyguy <divyanshu123123@gmail.com>
2023-09-18 04:17:53 +0530

    made clients responsive

--- components/clients/ClientCard.tsx
+++ components/clients/ClientCard.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 const ClientCard = ({client}:{client: Client}) =>{
     return (
-        <div className="w-[200px] inline-block px-3 relative mt-10 " >
+        <div className="w-[100px] md:w-[100px] inline-block px-0 md:px-3 relative mt-2 mx-0" >
             <Link href={client.twitterLink}>
-        <div className={`relative  align-middle w-[400pxcursor-pointer`}>                                        
+            <div className={`relative align-middle w-full cursor-pointer`}>                                        
             <div className="flex flex-wrap flex-col items-center sm:flex-nowrap sm:space-x-6" >
-                <div className="relative mt-1 h-30 w-30 flex-shrink-0">  
+                <div className="relative mt-1 h-30 w-30 flex-shrink-0 mb-0">  
                     <Image
                         src={client.profilePicture.url}
                         alt={client.userName}
@@ -17,7 +17,7 @@ const ClientCard = ({client}:{client: Client}) =>{
                         sizes="96px"
                     />
                 </div>
-                <div className="mb-3 h-24 flex items-center">
+                <div className=" h-24 flex items-center mt-0 py-0">
                     <h3 className="text-xl font-bold text-gray-800 dark:text-gray-300">
                         {client.userName}
                     </h3>
--- components/clients/ClientsSection.tsx
+++ components/clients/ClientsSection.tsx
@@ -3,7 +3,7 @@ import {Clients} from "@/components";
 const ClientsSection: React.FC = () => {
     return (
         <>
-        <div className="sm:px-0  px-2 py-2 xl:px-10 mx-6 w-full flex flex-col items-center justify-center mt-10">
+        <div className="px-0 md:px-2 py-2 xl:px-10 mx-0 md:mx-6 w-full flex flex-col items-center justify-center mt-10">
             <div className="w-full flex items-center justify-center">
                 <h2 className="text-3xl font-semibold stylish-font text-[#3C61BD] lg:text-4xl lg:leading-tight tracking-tighter">
                     Clients
