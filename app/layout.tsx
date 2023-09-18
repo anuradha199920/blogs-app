c9623af6b2472da67503c424aabab233ccbc3ffe
Eekeyguy <divyanshu123123@gmail.com>
2023-09-18 17:15:19 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { Inter } from 'next/font/google'
 import {NavBar} from "@/components";
 import React from "react";
 import { Providers } from './providers';
+import Script from 'next/script';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +18,24 @@ export const metadata: Metadata = {
 export default function RootLayout({children,}: { children: React.ReactNode }) {
     return (
     <html lang="en">
+        <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+       <Head>
+            <title>0x Not Fungible</title>
+       </Head>
         <body className={`${inter.className} bg-white dark:bg-[#434343]`}>
 
             <NavBar />
