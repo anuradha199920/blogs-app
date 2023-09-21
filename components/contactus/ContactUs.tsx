bf401345c9e261681a5beaff46ee69ce207cd296
Eekeyguy <divyanshu123123@gmail.com>
2023-09-22 03:35:38 +0530

    .

--- components/contactus/ContactUs.tsx
+++ components/contactus/ContactUs.tsx
@@ -4,7 +4,7 @@ const ContactUs = ()=>{
     return (
         <>
 
-        <div className="w-full md:w-[98%] m-0 my-4 md:m-4 flex flex-wrap items-center justify-center">
+        {/* <div className="w-full md:w-[98%] m-0 my-4 md:m-4 flex flex-wrap items-center justify-center">
             <div className="relative mt-1 h-20 w-20 flex-shrink-0 mb-0 rounded-full shadow-sm">
                 <Link href={`https://twitter.com/eekeyguy_eth`}>
                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="60" height="60" viewBox="0 0 50 50">
@@ -13,14 +13,14 @@ const ContactUs = ()=>{
                 </Link>
                 
             </div>
-            {/* <div className="relative mt-1 h-20 w-20 flex-shrink-0 mb-0">
+            <div className="relative mt-1 h-20 w-20 flex-shrink-0 mb-0">
                 <Link href={"https://mail.google.com/mail/u/0/?fs=1&to=official.0xnotfungible@gmail.com&tf=cm"}>
                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="60" height="60" viewBox="0 0 50 50">
                     <path fill="#4caf50" d="M45,16.2l-5,2.75l-5,4.75L35,40h7c1.657,0,3-1.343,3-3V16.2z"></path><path fill="#1e88e5" d="M3,16.2l3.614,1.71L13,23.7V40H6c-1.657,0-3-1.343-3-3V16.2z"></path><polygon fill="#e53935" points="35,11.2 24,19.45 13,11.2 12,17 13,23.7 24,31.95 35,23.7 36,17"></polygon><path fill="#c62828" d="M3,12.298V16.2l10,7.5V11.2L9.876,8.859C9.132,8.301,8.228,8,7.298,8h0C4.924,8,3,9.924,3,12.298z"></path><path fill="#fbc02d" d="M45,12.298V16.2l-10,7.5V11.2l3.124-2.341C38.868,8.301,39.772,8,40.702,8h0 C43.076,8,45,9.924,45,12.298z"></path>
                 </svg>
                 </Link>
-            </div> */}
-        </div>
+            </div>
+        </div> */}
         </>
     );
 };
